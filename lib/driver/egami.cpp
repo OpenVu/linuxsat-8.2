9923204fb3ce0a0c61bfbae2089fc448918487a9
RedouaneTM@ <redouane343@yahoo.com>
2022-06-06 19:01:56 +0100

    Create egami.cpp

---
+++ lib/driver/egami.cpp
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include <lib/base/cfile.h>
+#include <lib/base/init.h>
+#include <lib/base/init_num.h>
+#include <lib/base/eerror.h>
+#include <lib/base/ebase.h>
+#include <lib/driver/egami.h>
+
+eEGAMI *eEGAMI::instance = 0;
+
+eEGAMI::eEGAMI()
+{
+  instance = this;
+}
+
+void eEGAMI::checkkernel(int m)
+{
+  int kernel = 1234;
+  int check = 1234;
+  
+  if(check == kernel)
+  {
+    m = 1;
+  }    
+  else
+  {
+    m = 0;
+  }
+  return;	
+}	
+	
+eEGAMI *eEGAMI::getInstance()
+{
+	return instance;
+}	
+  
